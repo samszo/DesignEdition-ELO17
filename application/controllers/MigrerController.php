35c91760f934b640740a725503ee4eead15ec90f
Adjib Ahamada <ahamada.adjib@gmail.com>
2017-05-09 03:24:09 +0200

    modif migrercontroller
    modif du code

--- application/controllers/MigrerController.php
+++ application/controllers/MigrerController.php
@@ -11,15 +11,20 @@ class MigrerController extends Zend_Controller_Action
     public function indexAction()
     {
         $s = new Flux_Site();
-        
+
         $url = "http://gapai.univ-paris8.fr/DesignEdition/?page=articlejson";
         $this->view->data = $s->getUrlBodyContent($url,false,false);
         $this->view->arr = json_decode($this->view->data);
-        
+
     }
 
+    if($this->_getParam('csv')){
+    				foreach ($data as $s) {
+    					if(!$this->view->content)$this->view->content = $data->arrayToCsv(array_keys($s),",").PHP_EOL;
+    					$this->view->content .= $data->arrayToCsv($s,",").PHP_EOL;
+    				}
+    			}else
+    				$this->view->content = json_encode($data);
 
-    
-    
+
 }
-
